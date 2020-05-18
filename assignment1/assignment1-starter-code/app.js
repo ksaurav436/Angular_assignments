cc23225ea3d0e7e095365bde0dd104dc90457436
ksaurav436 <ksaurav436@gmail.com>
2020-05-18 22:59:20 +0530

    Answer

---
+++ assignment1/assignment1-starter-code/app.js
@@ -0,0 +1,30 @@
+(function(){
+'use strict'
+
+angular.module('Checker', [])
+
+.controller('LunchChecker', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+	$scope.message = "Go ahead"
+
+	$scope.Check = function(){
+		var arr = $scope.foods.split(",")
+
+		var count = 0
+		for (var i = arr.length - 1; i >= 0; i--) {
+			if (arr[i] != ""){
+				count += 1
+			}
+		}
+		if (count <= 3) {
+			$scope.message = "Enjoy!"
+		}
+		else{
+			$scope.message = "Too much!"
+		}
+	}
+}
+
+})();
